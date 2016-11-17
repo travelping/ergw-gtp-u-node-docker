4ace2624eb750b9a6310c30a028bf9cdb7cc9086
Tobias Famulla <tobias.famulla@travelping.com>
2016-11-17 11:32:16 +0100

    remove hardcoded version from path

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ COPY pre-start.sh /etc/ergw-gtp-u-node/
 COPY ergw-gtp-u-start.sh /usr/bin/
 COPY default-vars.sh /etc/ergw-gtp-u-node/
 # Copy templates
-COPY config/sys.config.templ /opt/rel/ergw-gtp-u-node/releases/0.1.0/
-COPY config/vm.args.templ /opt/rel/ergw-gtp-u-node/releases/0.1.0/
+COPY config/sys.config.templ /etc/ergw-gtp-u-node/
+COPY config/vm.args.templ /etc/ergw-gtp-u-node/
 
 CMD ["ergw-gtp-u-start.sh"]
--- ergw-gtp-u-start.sh
+++ ergw-gtp-u-start.sh
@@ -4,9 +4,11 @@ set -e
 
 . /etc/ergw-gtp-u-node/default-vars.sh
 #/etc/ergw-gtp-u-node/pre-start.sh
-cat /opt/rel/ergw-gtp-u-node/releases/0.1.0/sys.config.templ | \
-    envsubst > /opt/rel/ergw-gtp-u-node/releases/0.1.0/sys.config
-cat /opt/rel/ergw-gtp-u-node/releases/0.1.0/vm.args.templ | \
-    envsubst > /opt/rel/ergw-gtp-u-node/releases/0.1.0/vm.args
+# variable substitution in configuration files
+RELPATH=$(find /opt/rel/ergw-gtp-c-node/releases/ -mindepth 1 -maxdepth 1 -type d)
+cat /etc/ergw-gtp-u-node/sys.config.templ | \
+    envsubst > $RELPATH/sys.config
+cat /etc/ergw-gtp-u-node/vm.args.templ | \
+    envsubst > $RELPATH/vm.args
 
 /opt/rel/ergw-gtp-u-node/bin/ergw-gtp-u-node foreground
