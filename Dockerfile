7a85ea583de6cc3aea98984a2c6f6a2d2fc2158a
Tobias Famulla <tobias.famulla@travelping.com>
2016-11-17 18:10:04 +0100

    change architecture to use base image

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM grundrausch3n/ergw-docker-base
 
 WORKDIR /
 COPY _build/prod/rel/ /opt/rel/
-COPY pre-start.sh /etc/ergw-gtp-u-node/
+COPY pre-start.sh /config/ergw-gtp-u-node/
 COPY ergw-gtp-u-start.sh /usr/bin/
-COPY default-vars.sh /etc/ergw-gtp-u-node/
+COPY default-vars.sh /config/ergw-gtp-u-node/
 # Copy templates
-COPY config/sys.config.templ /etc/ergw-gtp-u-node/
-COPY config/vm.args.templ /etc/ergw-gtp-u-node/
+COPY config/sys.config.templ /config/ergw-gtp-u-node/
+COPY config/vm.args.templ /config/ergw-gtp-u-node/
 
 CMD ["ergw-gtp-u-start.sh"]
--- ergw-gtp-u-start.sh
+++ ergw-gtp-u-start.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-. /etc/ergw-gtp-u-node/default-vars.sh
+. /config/ergw-gtp-u-node/default-vars.sh
 #/etc/ergw-gtp-u-node/pre-start.sh
 # variable substitution in configuration files
 RELPATH=$(find /opt/rel/ergw-gtp-u-node/releases/ -mindepth 1 -maxdepth 1 -type d)
-cat /etc/ergw-gtp-u-node/sys.config.templ | \
+cat /config/ergw-gtp-u-node/sys.config.templ | \
     envsubst > $RELPATH/sys.config
-cat /etc/ergw-gtp-u-node/vm.args.templ | \
+cat /config/ergw-gtp-u-node/vm.args.templ | \
     envsubst > $RELPATH/vm.args
 
 /opt/rel/ergw-gtp-u-node/bin/ergw-gtp-u-node foreground
