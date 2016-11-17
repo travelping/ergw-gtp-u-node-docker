afeaa74c48672faf4bd4f23f38559b5272389bcd
Tobias Famulla <tobias.famulla@travelping.com>
2016-11-17 12:28:23 +0100

    fix typo

--- ergw-gtp-u-start.sh
+++ ergw-gtp-u-start.sh
@@ -5,7 +5,7 @@ set -e
 . /etc/ergw-gtp-u-node/default-vars.sh
 #/etc/ergw-gtp-u-node/pre-start.sh
 # variable substitution in configuration files
-RELPATH=$(find /opt/rel/ergw-gtp-c-node/releases/ -mindepth 1 -maxdepth 1 -type d)
+RELPATH=$(find /opt/rel/ergw-gtp-u-node/releases/ -mindepth 1 -maxdepth 1 -type d)
 cat /etc/ergw-gtp-u-node/sys.config.templ | \
     envsubst > $RELPATH/sys.config
 cat /etc/ergw-gtp-u-node/vm.args.templ | \
